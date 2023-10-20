83e12f0861b2ebde36013038ed256e1f7ad2585a
Venkatesh-Router <67518179+B-Venkatesh7210@users.noreply.github.com>
2023-10-20 20:47:12 +0530

    Unicrypt Tokens done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const {fetchPinkSaleTokens} = require('./controllers/fetchPinkSaleTokens');
+const {fetchUnicryptTokens} = require('./controllers/fetchUnicryptTokens');
+
 
 (async () => {
   try {
-    await fetchPinkSaleTokens();
+    // await fetchPinkSaleTokens();
+    await fetchUnicryptTokens();
   } catch (error) {
     console.error("An error occurred:", error);
   }
--- utils/fetchTransactions.js
+++ utils/fetchTransactions.js
@@ -6,7 +6,7 @@ const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
 async function fetchTransactions(contractAddress) {
     let page = 1;
     let transactions = [];
-    let blockNumber = 0; //TODO: Add block number of contract deployment
+    let blockNumber = 11463946; //TODO: Add block number of contract deployment
     while (true) {
       const params = {
         module: "account",
